ab17955bc769a7f30ad98886ab2cae9cffc343c6
jpaul-kr <jonpk555@gmail.com>
2024-11-27 13:03:45 +0100

    now i understand it and make it all work

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -17,3 +17,21 @@ int	main(int argc, char** argv)
 		std::cout << e.what() << std::endl;
 	}
 }
+/*		RECURSIVA:
+ *	IDA:
+ *ordenar por parejas -> groupsize *= 2;
+ hasta que solo haya un grupo del tamanyo de groupsize
+
+ 	VUELTA:
+ordenamos los grupos comparando con el numero mas grande de cada pareja de grupos (lo que seria la pareja b)
+luego se utiliza jacobs zahl x2 para anyadir las parejas a en funcion del indice de jacobs = 0 2 2 6 10 ...
+primero va la pareja a[0] indice: 0
+
+luego va el siguiente indice de la pareja (0 + 2 = 2): a[2]
+y despues va el indice - 1 : a[1]
+
+luego el siguiente que es (2 + 2 = 4): a[4]
+despues va el indice - 1 (4 - 1 = 3) : a[3]
+
+... asi sucesivamente.*/
+
